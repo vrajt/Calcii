b1b94d7dff85e5c5bcfff831fbf1469651845377
vrajt <vrajthaker10@gmail.com>
2023-08-22 14:35:45 +0530

    without javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+// let a=prompt("hey Whats your age ?")
+// a=Number.parseInt(a)
+// if (a>=18){
+//     alert("You are a valid user")
+// }
+// else{
+//     alert("ur age is not valid")
+// }
+
+alert("Can  you drive",(a<18?"you cannot drive drive":"you can drive"))
