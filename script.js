fdcb741aafea9e0fd09ee8983d7e514989b834fa
vrajt <vrajhaker10@gmail.com>
2023-08-23 18:37:22 +0530

    adding javascript incalculator UI

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
+let displayValue = '';// Variable to store the displayed value
 
+function display(value) {     //Function to add value on display
+    displayValue += value;
+    updateDisplay();
+}
+//function to clear display
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+}
+//fuction to remove a character
+function removeLastChar() {
+    displayValue = displayValue.slice(0, -1);
+    updateDisplay();
+}
+//function to update the value  on display
+ function updateDisplay() {
+     document.getElementById('display').value = displayValue;
+ }
+//function to calculate
+function calculate() {
+    let currentExpression = displayValue.split(/([+\-*/])/);  // it seperates numbers and expression
+    let result = parseFloat(currentExpression[0]);   //it initialize the result with firtst number
 
-// let a=prompt("hey Whats your age ?")
-// a=Number.parseInt(a)
-// if (a>=18){
-//     alert("You are a valid user")
-// }
-// else{
-//     alert("ur age is not valid")
-// }
-
-alert("Can  you drive",(a<18?"you cannot drive drive":"you can drive"))
+//iterate through array for performing calculation
+    for (let i = 1; i < currentExpression.length; i += 2) {
+        let operator = currentExpression[i];
+        let operand = parseFloat(currentExpression[i + 1]);
+//to check if it is operator
+        if (!isNaN(operand)) {
+            switch (operator) {
+                case '+':
+                    result += operand;
+                    break;
+                case '-':
+                    result -= operand;
+                    break;
+                case '*':
+                    result *= operand;
+                    break;
+                case '/':
+                    if (operand !== 0) {
+                        result /= operand;
+                    } else {
+                        result = 'Error';
+                        break;
+                    }
+                    break;
+            }
+        }
+    }
+//update the display with the result
+    displayValue = result.toString();
+    updateDisplay();
+}
