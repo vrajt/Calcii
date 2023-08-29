ee2294ce3fb16d20430945ef8b70b0161e14fa60
vrajt <vrajhaker10@gmail.com>
2023-08-29 18:54:34 +0530

    Fixing issues of fixing limits

--- script.js
+++ script.js
@@ -18,6 +18,28 @@ function removeLastChar() {
  function updateDisplay() {
      document.getElementById('display').value = displayValue;
  }
+ let calculationDone = false; // Flag to track if calculation has been done
+
+function display(value) {
+    if (!calculationDone) {
+        displayValue += value;
+        updateDisplay();
+    }
+}
+
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+    calculationDone = false; // Reset the flag
+}
+
+function removeLastChar() {
+    if (!calculationDone) {
+        displayValue = displayValue.slice(0, -1);
+        updateDisplay();
+    }
+}
+
 //function to calculate
 function calculate() {
     let currentExpression = displayValue.split(/([+\-*/])/);  // it seperates numbers and expression
@@ -39,18 +61,22 @@ function calculate() {
                 case '*':
                     result *= operand;
                     break;
-                case '/':
-                    if (operand !== 0) {
-                        result /= operand;
-                    } else {
-                        result = 'Error';
-                        break;
-                    }
+                    case '/':
+                        if (operand !== 0) {
+                            result /= operand;
+                        } else {
+                            result ="error";
+                            break;
+                        }
                     break;
             }
         }
     }
 //update the display with the result
+
+ result = parseFloat(result.toFixed(17));
     displayValue = result.toString();
     updateDisplay();
+    //calculationDone=true;
 }
+
